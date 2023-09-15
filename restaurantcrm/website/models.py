5de9d82abf85e47f27607549476dc3abe1b66ad2
David Mondragon <dragon18saiz@gmail.com>
2023-09-15 17:20:22 -0600

    Created Restaurant model, added proper fields (models.py)

--- restaurantcrm/website/models.py
+++ restaurantcrm/website/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(model.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    rest_name = models.CharField(max_length=50)
+    cuisine = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return(f"{self.rest_name} {self.cusine}")
