d57e12dbbf8d52a967ebd4bcd38025dc789b2488
David Mondragon <dragon18saiz@gmail.com>
2023-09-15 16:56:08 -0600

    Added form to register template

--- restaurantcrm/website/forms.py
+++ restaurantcrm/website/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
-class SignUpFrom(UserCreationForm):
+class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
     firs_name = forms.CharField(label="", max_length=80, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}))
     last_name = forms.CharField(label="", max_length=80, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}))
