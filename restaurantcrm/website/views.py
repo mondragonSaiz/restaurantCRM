5a48b1ce66a70f04bbdc183477ab46cbde2580f9
David Mondragon <dragon18saiz@gmail.com>
2023-09-15 15:14:10 -0600

    Added login form, is user auth logic

--- restaurantcrm/website/views.py
+++ restaurantcrm/website/views.py
@@ -4,8 +4,8 @@ from django.contrib import messages
 
 # Create your views here.
 def home(request):
+    
     return render(request, 'home.html', {})
-def login_user(request):
-    pass
+
 def logout_user(request):
     pass
