d7e240bb4cff98e032ca2ece05220fed6945b3f5
David Mondragon <dragon18saiz@gmail.com>
2023-09-15 18:34:33 -0600

    fixed field name in retunr self (models.py)

--- restaurantcrm/website/models.py
+++ restaurantcrm/website/models.py
@@ -9,4 +9,4 @@ class Restaurant(models.Model):
     phone = models.CharField(max_length=20)
 
     def __str__(self):
-        return(f"{self.rest_name} {self.cusine}")
+        return(f"{self.rest_name} {self.cuisine}")
