9e87a5011550d2b1500e8ead49df85b4163df38d
David Mondragon <dragon18saiz@gmail.com>
2023-09-15 21:25:23 -0600

    Added addrestaurat form in forms.py

--- restaurantcrm/website/forms.py
+++ restaurantcrm/website/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
+from .models import Restaurant
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="", widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Email Address'}))
@@ -29,3 +30,16 @@ class SignUpForm(UserCreationForm):
         self.fields['password2'].widget.attrs['placeholder'] = 'Confirm Password'
         self.fields['password2'].label = ''
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'	
+
+
+
+# Add restaurant form
+class AddRestaurantForm(forms.ModelForm):
+    rest_name = forms.CharField(required=True, widget=forms.widget.TextInput(attrs={"placeholder": "Name", "class": "form-control"}), label="")
+    cuisine = forms.CharField(required=True, widget=forms.widget.TextInput(attrs={"placeholder": "Restaurant type", "class": "form-control"}), label="")
+    address = forms.CharField(required=True, widget=forms.widget.TextInput(attrs={"placeholder": "Address", "class": "form-control"}), label="")
+    phone = forms.CharField(required=True, widget=forms.widget.TextInput(attrs={"placeholder": "Phone", "class": "form-control"}), label="")
+
+    class Meta:
+        model = Restaurant
+        exclude = ("user",)
